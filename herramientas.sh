481d00dfa00a9473357d20cf34c5499e5235df58
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2021-01-22 09:56:19 +0100

    Update herramientas.sh

--- herramientas.sh
+++ herramientas.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 # #################################################
 # ##     Instalación de herramientas para WP     ##
 # #################################################
@@ -18,5 +20,17 @@ wp plugin install --path=/var/www/html/ wp-githuber-md --activate --allow-root
 # En este caso hemos elegido el tema Bravada
 wp theme install --path=/var/www/html/ bravada --activate --allow-root
 
+# Instalamos Ruby
+apt install ruby -y
 
+# Instalamos dependencias de Ruby
+apt install build-essential libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev  libgmp-dev zlib1g-dev -y
+
+# Instalamos la herramienta de auditoria
+gem install wpscan
+# Actualizamos
+wpscan --update
+
+# Enumeramos los usuarios
+wpscan --url http://54.173.16.66 --enumerate u
 
